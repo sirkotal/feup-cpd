cd7aa27466da93958c166506edc6ff9529238626
sirkotal <lordkotal2002@gmail.com>
2024-02-07 11:24:12 +0000

    Fixed some errors

--- matrixproduct.rs
+++ matrixproduct.rs
@@ -1,13 +1,7 @@
-fn OnMult(m_ar: i64, m_br: i64) {
-    let mut pha = vec![f64; m_ar * m_ar];
-    let mut phb = vec![f64; m_br * m_br];
-    let mut phc = vec![f64; m_ar * m_br];
-
-    for i in 0..m_ar {
-        for j in 0..m_ar {
-            phb[i * m_ar + j] = (1.0) as f64;
-        }
-    }
+fn OnMult(m_ar: usize, m_br: usize) {
+    let mut pha = vec![1.0; m_ar * m_ar];
+    let mut phb = vec![0.0; m_br * m_br];
+    let mut phc = vec![0.0; m_ar * m_br];
 
     for i in 0..m_br {
         for j in 0..m_br {
