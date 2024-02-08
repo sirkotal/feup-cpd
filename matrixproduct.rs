406962905db6bfb309fb5f294a50497b1cfdfba5
sirkotal <lordkotal2002@gmail.com>
2024-02-08 15:03:53 +0000

    Implemented the OnMultLine in Rust

--- matrixproduct.rs
+++ matrixproduct.rs
@@ -34,3 +34,38 @@ fn OnMult(m_ar: usize, m_br: usize) {
 
     println!();
 }
+
+fn OnMultLine(m_ar: usize, m_br: usize) {
+    let mut pha = vec![1.0 as f64; m_ar * m_ar];
+    let mut phb = vec![0.0 as f64; m_ar * m_ar];
+    let mut phc = vec![0.0 as f64; m_ar * m_ar];
+
+    for i in 0..m_br {
+        for j in 0..m_br {
+            phb[i * m_br + j] = (i + 1) as f64;
+        }
+    }
+
+    let start = std::time::Instant::now();
+
+    for i in 0..m_ar {
+        for k in 0..m_ar {
+            for j in 0..m_br {
+                phc[i * m_ar + j] += pha[i * m_ar + k] * phb[k * m_br + j];
+            }
+        }
+    }
+
+    let end = std::time::Instant::now();
+    let duration = end - start;
+
+    println!("Time: {:.3} seconds", duration.as_secs_f64());
+
+    println!("Result matrix: ");
+
+    for j in 0..std::cmp::min(10, m_br) {
+        println!("{} ", phc[j]);
+    }
+
+    println!();
+}
