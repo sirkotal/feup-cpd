f1ba8a3e5efe56009f34823ba1d13a922cfbde28
sirkotal <lordkotal2002@gmail.com>
2024-02-20 11:02:33 +0000

    Initial implementation of the multi-core functions

--- src/matrix_mul.cpp
+++ src/matrix_mul.cpp
@@ -172,6 +172,123 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
     free(phc);
 }
 
+void OnMultLineP1(int m_ar, int m_br)
+{
+
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = clock();
+
+	#pragma omp parallel for
+	for (i = 0; i < m_ar; i++) {	
+		for (k = 0; k < m_ar; k++) {	
+			for (j = 0; j < m_br; j++) {	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++) {	
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+    
+}
+
+void OnMultLineP2(int m_ar, int m_br)
+{
+
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = clock();
+
+	#pragma omp parallel for
+	for (i = 0; i < m_ar; i++) {	
+		for (k = 0; k < m_ar; k++) {	
+			#pragma omp for
+			for (j = 0; j < m_br; j++) {	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++) {	
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+    
+}
+
 
 
 void handle_error (int retval)
