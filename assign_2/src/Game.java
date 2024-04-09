dc119b220bbf383d92ca2d79e21ba8d8465fcfcb
sirkotal <lordkotal2002@gmail.com>
2024-04-09 09:53:32 +0100

    Added the base code for the Game class

---
+++ assign_2/src/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+    private List<Socket> userSockets;
+
+    public Game(int players, List<Socket> userSockets) {
+        this.userSockets = userSockets;
+    }
+
+    public void start() {
+        // Code to start the game
+        System.out.println("Starting game with " + userSockets.size() + " players");
+    }
+}
